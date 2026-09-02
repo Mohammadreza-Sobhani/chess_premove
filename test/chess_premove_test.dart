98f5ec58ec54bcde57e44eaa7ed1622f5d265d38
Mohammadreza Sobhani <ir.mohammadreza.sobhani@gmail.com>
2026-09-02 07:35:13 +0000

    Translate Persian comments and messages to English

--- test/chess_premove_test.dart
+++ test/chess_premove_test.dart
@@ -4,7 +4,7 @@ import 'package:chess_premove/chess_premove.dart';
 void main() {
   group('PremoveIntelligence Tests', () {
     test('Initialization and basic logic test', () {
-      // استفاده از کلاس اصلی پکیج برای تایید صحت ایمپورت و رفع خطای عدم استفاده
+      // Using the main package class to verify import validity and fix unused import warnings
       expect(PremoveIntelligence, isNotNull);
     });
   });
